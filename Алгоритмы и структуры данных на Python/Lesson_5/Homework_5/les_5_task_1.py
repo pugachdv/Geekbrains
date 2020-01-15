59bcdfacb9aaa7e7802ba38fefa633a8fffffe57
pugach <pugachdv@gmail.com>
2020-01-15 11:46:06 +0300

    Готово

--- Алгоритмы и структуры данных на Python/Lesson_5/Homework_5/les_5_task_1.py
+++ Алгоритмы и структуры данных на Python/Lesson_5/Homework_5/les_5_task_1.py
@@ -1,23 +1,45 @@
-# 1. Пользователь вводит данные о количестве предприятий, их наименования и прибыль за четыре квартала для каждого предприятия.
-# Программа должна определить среднюю прибыль (за год для всех предприятий)
-# и отдельно вывести наименования предприятий, чья прибыль выше среднего и ниже среднего.
+#Пользователь вводит данные о количестве предприятий, их наименования и прибыль за четыре квартала для каждого предприятия.
+# Программа должна определить среднюю прибыль (за год для всех предприятий) и отдельно вывести наименования предприятий,
+# чья прибыль выше среднего и ниже среднего.
 
-from collections import namedtuple
+import collections
+import random
 
-k = int(input('Введите количество предприятий'))
-New_Company = namedtuple('New_Company', 'name, first, second, third, fourth', defaults=[0, 0, 0, 0])
+Enterprise = collections.namedtuple('Enterprise', ['q1', 'q2', 'q3', 'q4'])
 
-for i in range(1, k + 1):
+base_enterprise = {}
 
-    i = New_Company([input('Название'), float(input('1-ый квартал')), float(input('2-ой квартал')), float(input('3-ий квартал')), float(input('4-ый квартал'))])
+n = int(input("Количество предприятий: "))
 
+for i in range(n):
+    name = input('Наименование ' + str(i+1) + '-го предприятия: ')
+    profit_q1 = int(input('Прибыль за 1-й квартал: '))
+    profit_q2 = int(input('Прибыль за 2-й квартал: '))
+    profit_q3 = int(input('Прибыль за 3-й квартал: '))
+    profit_q4 = int(input('Прибыль за 4-й квартал: '))
+    base_enterprise[name] = Enterprise(
+        q1=profit_q1,
+        q2=profit_q2,
+        q3=profit_q3,
+        q4=profit_q4
+    )
 
+total_profit = ()
 
+for name, profit in base_enterprise.items():
+    print(f'Предприятие: {name} прибыль за год - {sum(profit)}')
+    total_profit += profit
 
-#     enterprises[name].append(enterprises[name][1] / enterprises[name][0])
-#
-# print('Фактическая прибыль больше 10, но план не выполнен(<100%)')
-#
-# for key, value in enterprises.items():
-#     if value[1] > 10 and value[2] < 1:
-#         print(f'Предприятие {key} заработало {value[1]}, что составило {value[2] * 100:.2f}%')
+avg_profit_total = sum(total_profit) / len(base_enterprise)
+print(f'Средняя прибыль за год для всех предприятий {avg_profit_total}')
+
+print('Предприятия, у которых прибыль выше среднего:')
+
+for name, profit in base_enterprise.items():
+    if sum(profit) > avg_profit_total:
+        print(f'{name} - {sum(profit)}')
+
+print('Предприятия, у которых прибыль ниже среднего:')
+for name, profit in base_enterprise.items():
+    if sum(profit) < avg_profit_total:
+        print(f'{name} - {sum(profit)}')
