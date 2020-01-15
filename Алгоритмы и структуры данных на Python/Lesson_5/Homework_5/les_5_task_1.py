ec6bdf9805e16fd2b8c97725bb39f1a501efb215
pugach <pugachdv@gmail.com>
2020-01-15 11:50:52 +0300

    Готово

--- Алгоритмы и структуры данных на Python/Lesson_5/Homework_5/les_5_task_1.py
+++ Алгоритмы и структуры данных на Python/Lesson_5/Homework_5/les_5_task_1.py
@@ -1,45 +1,44 @@
 #Пользователь вводит данные о количестве предприятий, их наименования и прибыль за четыре квартала для каждого предприятия.
-# Программа должна определить среднюю прибыль (за год для всех предприятий) и отдельно вывести наименования предприятий,
-# чья прибыль выше среднего и ниже среднего.
+# Программа должна определить среднюю прибыль (за год для всех предприятий)
+# и отдельно вывести наименования предприятий, чья прибыль выше среднего и ниже среднего.
 
 import collections
-import random
 
 Enterprise = collections.namedtuple('Enterprise', ['q1', 'q2', 'q3', 'q4'])
 
-base_enterprise = {}
+template_enterprise = {}
 
 n = int(input("Количество предприятий: "))
 
 for i in range(n):
     name = input('Наименование ' + str(i+1) + '-го предприятия: ')
-    profit_q1 = int(input('Прибыль за 1-й квартал: '))
-    profit_q2 = int(input('Прибыль за 2-й квартал: '))
-    profit_q3 = int(input('Прибыль за 3-й квартал: '))
-    profit_q4 = int(input('Прибыль за 4-й квартал: '))
-    base_enterprise[name] = Enterprise(
-        q1=profit_q1,
-        q2=profit_q2,
-        q3=profit_q3,
-        q4=profit_q4
+    revenue_q1 = int(input('Прибыль за 1-й квартал: '))
+    revenue_q2 = int(input('Прибыль за 2-й квартал: '))
+    prevenue_q3 = int(input('Прибыль за 3-й квартал: '))
+    revenue_q4 = int(input('Прибыль за 4-й квартал: '))
+    template_enterprise[name] = Enterprise(
+        q1=revenue_q1,
+        q2=revenue_q2,
+        q3=revenue_q3,
+        q4=revenue_q4
     )
 
-total_profit = ()
+total_revenue = ()
 
-for name, profit in base_enterprise.items():
-    print(f'Предприятие: {name} прибыль за год - {sum(profit)}')
-    total_profit += profit
+for name, revenue in template_enterprise.items():
+    print(f'Предприятие: {name} прибыль за год - {sum(revenue)}')
+    total_revenue += revenue
 
-avg_profit_total = sum(total_profit) / len(base_enterprise)
-print(f'Средняя прибыль за год для всех предприятий {avg_profit_total}')
+avg_revenue_total = sum(total_revenue) / len(template_enterprise)
+print(f'Средняя прибыль за год для всех предприятий {avg_revenue_total}')
 
 print('Предприятия, у которых прибыль выше среднего:')
 
-for name, profit in base_enterprise.items():
-    if sum(profit) > avg_profit_total:
-        print(f'{name} - {sum(profit)}')
+for name, revenue in template_enterprise.items():
+    if sum(revenue) > avg_revenue_total:
+        print(f'{name} - {sum(revenue)}')
 
 print('Предприятия, у которых прибыль ниже среднего:')
-for name, profit in base_enterprise.items():
-    if sum(profit) < avg_profit_total:
-        print(f'{name} - {sum(profit)}')
+for name, revenue in template_enterprise.items():
+    if sum(revenue) < avg_revenue_total:
+        print(f'{name} - {sum(revenue)}')
